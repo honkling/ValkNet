8905da93a2b48db25cbc9132c1f64c5e86b61eeb
honkling <thehjonkening@gmail.com>
2021-04-28 15:51:25 -0500

    Add boilerplate

---
+++ src/main/java/me/honkling/valknet/Valknet.java
@@ -0,0 +1,17 @@
+package me.honkling.valknet;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Valknet extends JavaPlugin {
+
+	@Override
+	public void onEnable() {
+		// Plugin startup logic
+
+	}
+
+	@Override
+	public void onDisable() {
+		// Plugin shutdown logic
+	}
+}
